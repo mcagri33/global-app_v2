8e74aa221acaa91e44f9d11afd617c738355e088
cagri <mehmetcagrikaratas@gmail.com>
2022-11-30 16:49:13 +0300

    updated row count

--- resources/views/site/new/new_index.blade.php
+++ resources/views/site/new/new_index.blade.php
@@ -27,15 +27,15 @@
             <div class="row">
                 <!-- end col-3 -->
                 @foreach($posts as $p)
-
-                <div class="col-md-3">
-                    <div class="news-box"> <small>{{$p->bCategory->name}}</small>
-                        <h4>{{$p->title}}</h4>
-                        <span>{{$p->created_at->format('F Y')}}</span>
-                        <p>{{ Str::limit(strip_tags($p->description), 100) }}</p>
-                        <a href="{{route('site.new.detail',$p->slug)}}">{{Read_More}}</a> </div>
-                    <!-- end news-box -->
-                </div>
+                        <div class="col-md-4">
+                            <div class="news-box"><small>{{$p->bCategory->name}}</small>
+                                <h4>{{$p->title}}</h4>
+                                <span>{{$p->created_at->format('F Y')}}</span>
+                                <p>{{ Str::limit(strip_tags($p->description), 100) }}</p>
+                                <a href="{{route('site.new.detail',$p->slug)}}">{{Read_More}}</a>
+                            </div>
+                        <!-- end news-box -->
+                    </div>
                 @endforeach
                 <!-- end col-3 -->
                 <!-- end col-3 -->
@@ -49,69 +49,69 @@
         <!-- end container -->
     </section>
 
-   {{-- <input type="hidden" id="start" value="0">
+    {{-- <input type="hidden" id="start" value="0">
 
 
-    <!-- Script -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript">
+     <!-- Script -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+     <script type="text/javascript">
 
-        checkWindowSize();
+         checkWindowSize();
 
-        // Check if the page has enough content or not. If not then fetch records
-        function checkWindowSize(){
-            if($(window).height() >= $(document).height()){
-                // Fetch records
-                fetchData();
-            }
-        }
+         // Check if the page has enough content or not. If not then fetch records
+         function checkWindowSize(){
+             if($(window).height() >= $(document).height()){
+                 // Fetch records
+                 fetchData();
+             }
+         }
 
-        // Fetch records
-        function fetchData(){
-            var start = Number($('#start').val());
-            var allcount = Number($('#totalrecords').val());
-            var rowperpage = Number($('#rowperpage').val());
-            start = start + rowperpage;
+         // Fetch records
+         function fetchData(){
+             var start = Number($('#start').val());
+             var allcount = Number($('#totalrecords').val());
+             var rowperpage = Number($('#rowperpage').val());
+             start = start + rowperpage;
 
-            if(start <= allcount){
-                $('#start').val(start);
+             if(start <= allcount){
+                 $('#start').val(start);
 
-                $.ajax({
-                    url:"{{route('getPosts')}}",
-                    data: {start:start},
-                    dataType: 'json',
-                    success: function(response){
+                 $.ajax({
+                     url:"{{route('getPosts')}}",
+                     data: {start:start},
+                     dataType: 'json',
+                     success: function(response){
 
-                        // Add
-                        $(".post:last").after(response.html).show().fadeIn("slow");
+                         // Add
+                         $(".post:last").after(response.html).show().fadeIn("slow");
 
-                        // Check if the page has enough content or not. If not then fetch records
-                        checkWindowSize();
-                    }
-                });
-            }
-        }
+                         // Check if the page has enough content or not. If not then fetch records
+                         checkWindowSize();
+                     }
+                 });
+             }
+         }
 
-        $(document).on('touchmove', onScroll); // for mobile
+         $(document).on('touchmove', onScroll); // for mobile
 
-        function onScroll(){
+         function onScroll(){
 
-            if($(window).scrollTop() > $(document).height() - $(window).height()-100) {
+             if($(window).scrollTop() > $(document).height() - $(window).height()-100) {
 
-                fetchData();
-            }
-        }
+                 fetchData();
+             }
+         }
 
-        $(window).scroll(function(){
+         $(window).scroll(function(){
 
-            var position = $(window).scrollTop();
-            var bottom = $(document).height() - $(window).height();
+             var position = $(window).scrollTop();
+             var bottom = $(document).height() - $(window).height();
 
-            if( position == bottom ){
-                fetchData();
-            }
+             if( position == bottom ){
+                 fetchData();
+             }
 
-        });
+         });
 
-    </script>--}}
+     </script>--}}
 @endsection
