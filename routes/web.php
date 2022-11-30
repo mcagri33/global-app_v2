e7b200677cd25baa674d6ea78c485b5ecd25760e
cagri <mehmetcagrikaratas@gmail.com>
2022-11-30 13:56:02 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Castle\CastleController;
 use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Castle\CastleUserController;
+use App\Http\Controllers\Castle\CastleLanguageController;
+use App\Http\Controllers\Castle\CastleBlogCategoryController;
+use App\Http\Controllers\Castle\CastleBlogPostController;
+use App\Http\Controllers\Site\SiteLanguageController;
+use App\Http\Controllers\Site\SiteBlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,8 +39,59 @@ Route::group(['prefix' => 'castle/user', 'middleware' => 'auth'], function (){
         ->name('castle.user.delete');
 });
 
+Route::group(['prefix' => 'castle/bcategory', 'middleware' => 'auth'], function (){
+    Route::get('/',[CastleBlogCategoryController::class,'index'])
+        ->name('castle.bcategory.index');
+    Route::post('/store',[CastleBlogCategoryController::class,'store'])
+        ->name('castle.bcategory.store');
+    Route::get('/edit/{id}',[CastleBlogCategoryController::class,'edit'])
+        ->name('castle.bcategory.edit');
+    Route::post('/update',[CastleBlogCategoryController::class,'update'])
+        ->name('castle.bcategory.update');
+    Route::get('/delete/{id}',[CastleBlogCategoryController::class,'destroy'])
+        ->name('castle.bcategory.delete');
+});
 
+Route::group(['prefix' => 'castle/bpost', 'middleware' => 'auth'], function (){
+    Route::get('/',[CastleBlogPostController::class,'index'])
+        ->name('castle.bpost.index');
+    Route::post('/store',[CastleBlogPostController::class,'store'])
+        ->name('castle.bpost.store');
+    Route::get('/edit/{id}',[CastleBlogPostController::class,'edit'])
+        ->name('castle.bpost.edit');
+    Route::post('/update',[CastleBlogPostController::class,'update'])
+        ->name('castle.bpost.update');
+    Route::get('/delete/{id}',[CastleBlogPostController::class,'destroy'])
+        ->name('castle.bpost.delete');
+});
+
+Route::group(['prefix' => 'castle/language','middleware'=>'auth'], function (){
+    Route::get('/',[CastleLanguageController::class,'index'])
+        ->name('castle.language.index');
+    Route::get('/add',[CastleLanguageController::class,'create'])
+        ->name('castle.language.add');
+    Route::post('/store',[CastleLanguageController::class,'store'])
+        ->name('castle.language.store');
+    Route::get('/edit/{id}',[CastleLanguageController::class,'edit'])
+        ->name('castle.language.edit');
+    Route::post('/update',[CastleLanguageController::class,'update'])
+        ->name('castle.language.update');
+    Route::get('/delete/{id}',[CastleLanguageController::class,'destroy'])
+        ->name('castle.language.delete');
+    Route::get('/translation/{id}',[CastleLanguageController::class,'translation'])
+        ->name('castle.translation.index');
+    Route::post('/translation/update/{id}',[CastleLanguageController::class,'translation_update'])
+        ->name('castle.translation.update');
+
+});
 
 Route::get('/',[SiteController::class,'index'])
-    ->name('app.index');
+    ->name('site.index');
+Route::post('/language',[SiteLanguageController::class,'swich_language'])
+    ->name('front_language');
+Route::get('/news',[SiteBlogController::class,'index'])
+    ->name('site.new.index');
+Route::get('/{slug_post}',[SiteBlogController::class,'details'])
+    ->name('site.new.detail');
+
 require __DIR__.'/auth.php';
